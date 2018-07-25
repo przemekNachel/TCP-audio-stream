46a87a605cb9c0930b06dc6fb5204e08d95f5539
przemekNachel <przemekNachel@gmail.com>
2018-07-25 23:45:34 +0200

    Add Stream class and dataStructures

---
+++ src/main/java/codecool/Stream.java
@@ -0,0 +1,13 @@
+package codecool;
+
+import codecool.structures.MyQueue;
+
+public class Stream {
+
+    private int bitDepth = 16;
+    private int sampleRate = 44100;
+
+
+    MyQueue<Integer> sound = new MyQueue<>();
+
+}
