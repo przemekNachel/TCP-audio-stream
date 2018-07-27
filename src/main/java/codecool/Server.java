9d3b37f7455a88dd288be59f04194d0eca0001ef
przemekNachel <przemekNachel@gmail.com>
2018-07-27 17:36:41 +0200

    Move play sound to server

--- src/main/java/codecool/Server.java
+++ src/main/java/codecool/Server.java
@@ -2,6 +2,10 @@ package codecool;
 
 import codecool.structures.ReadMic;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -30,10 +34,31 @@ public class Server {
                 InputStream is = socket.getInputStream();
                 byte[] buf = new byte[ReadMic.BUFFER_SIZE];
                 is.read(buf, 0, buf.length);
-                ReadMic.playSound(buf);
+                playSound(buf);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void playSound (byte[] audio) throws Exception {
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(audio);
+        AudioInputStream audioStream = new AudioInputStream(bis, ReadMic.format, audio.length);
+        SourceDataLine sourceLine = AudioSystem.getSourceDataLine(ReadMic.format);
+
+        sourceLine.open(ReadMic.format);
+        sourceLine.start();
+
+        int nBytesRead = 1;
+        byte[] abData = new byte[ReadMic.BUFFER_SIZE];
+        while (nBytesRead != -1) {
+            try {
+                nBytesRead = audioStream.read(abData, 0, abData.length);
+                if (nBytesRead >= 0) sourceLine.write(abData, 0, nBytesRead);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
--- src/main/java/codecool/structures/ReadMic.java
+++ src/main/java/codecool/structures/ReadMic.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class ReadMic {
 
     public static AudioFormat format = new AudioFormat(44100, 8, 1, true, false);
-    public static int BUFFER_SIZE = 4096;
+    public static int BUFFER_SIZE = 12800;
 
 
     public static void main(String[] args) {
@@ -31,24 +31,5 @@ public class ReadMic {
     }
 
 
-    public static void playSound (byte[] audio) throws Exception {
 
-        ByteArrayInputStream bis = new ByteArrayInputStream(audio);
-        AudioInputStream audioStream = new AudioInputStream(bis, ReadMic.format, audio.length);
-        SourceDataLine sourceLine = AudioSystem.getSourceDataLine(format);
-
-        sourceLine.open(format);
-        sourceLine.start();
-
-        int nBytesRead = 1;
-        byte[] abData = new byte[BUFFER_SIZE];
-        while (nBytesRead != -1) {
-            try {
-                nBytesRead = audioStream.read(abData, 0, abData.length);
-                if (nBytesRead >= 0) sourceLine.write(abData, 0, nBytesRead);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
