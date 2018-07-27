2dc0dd28e9deb690688a08f8311a57aba9c67df8
przemekNachel <przemekNachel@gmail.com>
2018-07-27 16:47:51 +0200

    Change method play

--- src/main/java/codecool/structures/ReadMic.java
+++ src/main/java/codecool/structures/ReadMic.java
@@ -7,26 +7,19 @@ import java.io.IOException;
 
 public class ReadMic {
 
-    public static AudioFormat format = new AudioFormat(44100, 16, 2, true, false);
-    public static int BUFFER_SIZE = 1024;
+    public static AudioFormat format = new AudioFormat(44100, 8, 1, true, false);
+    public static int BUFFER_SIZE = 4096;
 
 
     public static void main(String[] args) {
-        byte[] a = getAudio();
 
-        try {
-            playSound(a);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static byte[] getAudio() {
         byte[] data = new byte[BUFFER_SIZE];
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
         try {
-            TargetDataLine line;
-            line = (TargetDataLine) AudioSystem.getLine(info);
+            TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
             line.open(format);
             line.start();
             line.read(data, 0, data.length);
@@ -42,25 +35,19 @@ public class ReadMic {
 
         ByteArrayInputStream bis = new ByteArrayInputStream(audio);
         AudioInputStream audioStream = new AudioInputStream(bis, ReadMic.format, audio.length);
+        SourceDataLine sourceLine = AudioSystem.getSourceDataLine(format);
 
-        AudioFormat audioFormat = null;
-        SourceDataLine sourceLine = null;
-
-        audioFormat = audioStream.getFormat();
-
-        sourceLine = AudioSystem.getSourceDataLine(audioFormat);
-        sourceLine.open(audioFormat);
+        sourceLine.open(format);
         sourceLine.start();
 
-        int nBytesRead = 0;
+        int nBytesRead = 1;
         byte[] abData = new byte[BUFFER_SIZE];
         while (nBytesRead != -1) {
             try {
                 nBytesRead = audioStream.read(abData, 0, abData.length);
+                if (nBytesRead >= 0) sourceLine.write(abData, 0, nBytesRead);
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            if (nBytesRead >= 0) { int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
             }
         }
     }
