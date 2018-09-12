fc81a9b6ece0391365961d17486c59d632c3d841
przemekNachel <przemekNachel@gmail.com>
2018-09-12 20:33:47 +0200

    Add choice of source data line

--- src/main/java/codecool/Client.java
+++ src/main/java/codecool/Client.java
@@ -2,6 +2,7 @@ package codecool;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 import javax.sound.sampled.*;
 
 public class Client {
@@ -17,7 +18,7 @@ public class Client {
 
 	private void start() {
 		try {
-			SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(Format.format);
+			SourceDataLine sourceDataLine = getSourceDataLine();
 			sourceDataLine.open(Format.format);
 			sourceDataLine.start();
 			Socket socket = new Socket(host, port);
@@ -28,6 +29,22 @@ public class Client {
 		} catch (IOException | LineUnavailableException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private SourceDataLine getSourceDataLine() throws LineUnavailableException {
+		int positon = 1;
+		for (Mixer.Info info : AudioSystem.getMixerInfo()) {
+			Line.Info[] linesInfo = AudioSystem.getMixer(info).getSourceLineInfo();
+			for (Line.Info lineInfo : linesInfo) {
+				Line line = AudioSystem.getLine(lineInfo);
+				if (line instanceof SourceDataLine) System.out.println(positon++ + ". " + info.getName());
+			}
+		}
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		int choice = 0;
+		if (input.matches("\\d+") && Integer.parseInt(input) < positon) choice = Integer.parseInt(input) - 1;
+		return (SourceDataLine) AudioSystem.getMixer(AudioSystem.getMixerInfo()[choice]).getLine(new DataLine.Info(SourceDataLine.class, Format.format));
 	}
 
 	public static void main(String[] args) {
