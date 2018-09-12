57d5a6660c9b44090d8fcafd8ea92fa45474b6d3
przemekNachel <przemekNachel@gmail.com>
2018-09-12 20:38:54 +0200

    Add choice of target data line

--- src/main/java/codecool/Server.java
+++ src/main/java/codecool/Server.java
@@ -3,6 +3,7 @@ package codecool;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import javax.sound.sampled.*;
 
 public class Server {
@@ -16,11 +17,10 @@ public class Server {
         byte tempBuffer[] = new byte[Format.BUFFER_SIZE];
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try {
+            TargetDataLine targetDataLine = getTargetDataLine();
             ServerSocket serverSocket = new ServerSocket(port);
             Socket socket = serverSocket.accept();
             BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-            DataLine.Info info = new DataLine.Info(TargetDataLine.class, Format.format);
-            TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(info);
             targetDataLine.open(Format.format);
             targetDataLine.start();
             while (true) {
@@ -31,6 +31,22 @@ public class Server {
         } catch (LineUnavailableException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private TargetDataLine getTargetDataLine() throws LineUnavailableException {
+        int positon = 1;
+        for (Mixer.Info info : AudioSystem.getMixerInfo()) {
+            Line.Info[] linesInfo = AudioSystem.getMixer(info).getTargetLineInfo();
+            for (Line.Info lineInfo : linesInfo) {
+                Line line = AudioSystem.getLine(lineInfo);
+                if (line instanceof TargetDataLine) System.out.println(positon++ + ". " + info.getName());
+            }
+        }
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int choice = 0;
+        if (input.matches("\\d+") && Integer.parseInt(input) < positon) choice = Integer.parseInt(input) - 1;
+        return (TargetDataLine) AudioSystem.getMixer(AudioSystem.getMixerInfo()[choice]).getLine(new DataLine.Info(TargetDataLine.class, Format.format));
     }
 
     public static void main(String[] args) {
