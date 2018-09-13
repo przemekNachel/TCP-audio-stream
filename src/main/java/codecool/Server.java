00a285bce04cd717eaeb2abc06567d16c0236e94
przemekNachel <przemekNachel@gmail.com>
2018-09-13 11:31:14 +0200

    Add multi-client handling

--- src/main/java/codecool/Server.java
+++ src/main/java/codecool/Server.java
@@ -3,30 +3,30 @@ package codecool;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sound.sampled.*;
 
 public class Server {
+
     private int port;
+    private static final List<BufferedOutputStream> clients = new ArrayList<>();
+    private byte[] tempBuffer;
+    private TargetDataLine targetDataLine;
 
     private Server(int port)  {
         this.port = port;
     }
 
     private void start() {
-        byte tempBuffer[] = new byte[Format.BUFFER_SIZE];
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        tempBuffer = new byte[Format.BUFFER_SIZE];
         try {
-            TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, Format.format));
+            targetDataLine = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, Format.format));
             ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
-            BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
             targetDataLine.open(Format.format);
             targetDataLine.start();
             while (true) {
-                int cnt = targetDataLine.read(tempBuffer, 0, tempBuffer.length);
-                out.write(tempBuffer);
-                if (cnt > 0) byteArrayOutputStream.write(tempBuffer, 0, cnt);
+                new IncomingRequestHandler(serverSocket.accept(), this).start();
             }
         } catch (LineUnavailableException | IOException e) {
             e.printStackTrace();
@@ -35,5 +35,42 @@ public class Server {
 
     public static void main(String[] args) {
         new Server(7575).start();
+    }
+
+    private static class IncomingRequestHandler extends Thread {
+
+        private final Socket socket;
+        private final Server server;
+
+        public IncomingRequestHandler(Socket socket, Server server) {
+            this.server = server;
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+
+                synchronized (clients) {
+                    clients.add(out);
+                }
+
+                while (true) {
+                    server.targetDataLine.read(server.tempBuffer, 0, server.tempBuffer.length);
+                    clients.forEach(bufferedOutputStream -> {
+                        try {
+                            bufferedOutputStream.write(server.tempBuffer);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
